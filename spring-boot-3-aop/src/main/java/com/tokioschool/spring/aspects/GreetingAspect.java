38512a0b2110f57f5d3d588780c6e0db61d02d82
Andrés Ruiz Peñuela <andres.rpenuela@babelgroup.com>
2025-02-11 14:26:12 +0100

    [sts6] example after about method

--- spring-boot-3-aop/src/main/java/com/tokioschool/spring/aspects/GreetingAspect.java
+++ spring-boot-3-aop/src/main/java/com/tokioschool/spring/aspects/GreetingAspect.java
@@ -3,6 +3,7 @@ package com.tokioschool.spring.aspects;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 @Aspect
 @Component
 public class GreetingAspect {
+	
 	//execution(Return package.class/inter.metodo(args) >> return puede ser {* (a todo), tipo de objeto especifico)
 	@Before(value = "execution(String com.tokioschool.spring.services.GreetingService.sayHello(..))") // Punto de corte, si es una interfaz se aplica a cualquier clase que implemente una interfza
 	public void loggerBefore(JoinPoint joinPoint) { // Consejo
@@ -20,5 +22,14 @@ public class GreetingAspect {
 		final String args = Arrays.toString(joinPoint.getArgs());
 		
 		log.info("Antes: "+method+" con los argumentos "+args);
+	}
+	
+	
+	@After(value = "execution(String com.tokioschool.spring.services.GreetingService.sayHello(..))") // Punto de corte, si es una interfaz se aplica a cualquier clase que implemente una interfza
+	public void loggerAfter(JoinPoint joinPoint) { // Consejo
+		final String method = joinPoint.getSignature().getName();
+		final String args = Arrays.toString(joinPoint.getArgs());
+		
+		log.info("Despues: "+method+" con los argumentos "+args);
 	}
 }
