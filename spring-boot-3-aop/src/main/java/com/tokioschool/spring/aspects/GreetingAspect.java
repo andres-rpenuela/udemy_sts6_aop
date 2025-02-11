8c6c05d3ce904c8215babc1ea73b3bda85444bfa
Andrés Ruiz Peñuela <andres.rpenuela@babelgroup.com>
2025-02-11 16:06:20 +0100

    [sts6] example around (before + after) about method, more after returning and After Throwing

--- spring-boot-3-aop/src/main/java/com/tokioschool/spring/aspects/GreetingAspect.java
+++ spring-boot-3-aop/src/main/java/com/tokioschool/spring/aspects/GreetingAspect.java
@@ -3,7 +3,11 @@ package com.tokioschool.spring.aspects;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -16,7 +20,7 @@ import lombok.extern.slf4j.Slf4j;
 public class GreetingAspect {
 	
 	//execution(Return package.class/inter.metodo(args) >> return puede ser {* (a todo), tipo de objeto especifico)
-	@Before(value = "execution(String com.tokioschool.spring.services.GreetingService.sayHello(..))") // Punto de corte, si es una interfaz se aplica a cualquier clase que implemente una interfza
+	/*@Before(value = "execution(String com.tokioschool.spring.services.GreetingService.sayHello(..))") // Punto de corte, si es una interfaz se aplica a cualquier clase que implemente una interfza
 	public void loggerBefore(JoinPoint joinPoint) { // Consejo
 		final String method = joinPoint.getSignature().getName();
 		final String args = Arrays.toString(joinPoint.getArgs());
@@ -31,5 +35,40 @@ public class GreetingAspect {
 		final String args = Arrays.toString(joinPoint.getArgs());
 		
 		log.info("Despues: "+method+" con los argumentos "+args);
+	}*/
+	
+	// reemplaza el before y after
+	@Around(value = "execution(String com.tokioschool.spring.services.GreetingService.sayHello(..))") // Punto de corte, si es una interfaz se aplica a cualquier clase que implemente una interfza
+	public Object loggerAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable { // Consejo
+		// before
+		final String method = proceedingJoinPoint.getSignature().getName();
+		final String args = Arrays.toString(proceedingJoinPoint.getArgs());
+		
+		log.info("@Around: "+method+" con los argumentos "+args);
+		
+		// ejecuta el meotod
+		Object result = proceedingJoinPoint.proceed();
+		
+		//after 
+		return result;
 	}
+	
+	// despues del return, join point para cualquier metodo de GreetingService, que tenga como parametro inicila un string
+	@AfterReturning("execution(String com.tokioschool.spring.services.GreetingService.*(String,..))")
+	public void loggerAfterReturning(JoinPoint joinPoint) {
+		final String method = joinPoint.getSignature().getName();
+		final String args = Arrays.toString(joinPoint.getArgs());
+		
+		log.info("@AfterReturning: "+method+" con los argumentos "+args);
+	}
+	
+	// despues del Throwin, join point para cualquier metodo de GreetingService que genere una excepcion
+	@AfterThrowing("execution(* com.tokioschool.spring.services.GreetingService.*(..))")
+	public void loggerThrowing(JoinPoint joinPoint) {
+		final String method = joinPoint.getSignature().getName();
+		final String args = Arrays.toString(joinPoint.getArgs());
+		
+		log.info("@AfterThrowing: "+method+" con los argumentos "+args);
+	}
+	
 }
